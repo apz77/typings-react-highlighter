a6a25bd238dd75c8f181b5e9aa9d269b846e532d
star__hoshi <kensuke1751@gmail.com>
2016-02-21 19:40:31 +0900

    fix test.

--- test.tsx
+++ test.tsx
@@ -1,7 +1,7 @@
 /// <reference path="typings/main.d.ts" />
 /// <reference path="react-highlighter.d.ts" />
 import * as React from "react";
-import {Highlighter} from "react-highlighter";
+import * as Highlighter from "react-highlighter";
 
 function highlighter() {
   return (
